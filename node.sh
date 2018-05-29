90394367c0e88f486141694c1933c2fd89a2e8a5
Nicolas Marien <nmarien29@gmail.com>
2018-05-29 22:04:32 +0200

    🍱 - Add some missing things

---
+++ node.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+npm i -g alfred-npms
+npm i -g alfred-dark-mode
+npm i -g alfred-npms
+npm i -g create-react-app
+npm i -g eslint
+npm i -g gatsby-cli
+npm i -g gitmoji-cli
+npm i -g http-server
+npm i -g rimraf
